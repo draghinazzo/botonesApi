1d57202a61021dfb6721bb2e9ef03ad7ac200308
Enrique <ajawab@gmail.com>
2025-03-04 15:33:07 -0600

    cambio en  el front

--- app/Models/Entrega.php
+++ app/Models/Entrega.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Entrega extends Model
 {
-    //
+    /**
+     * Los campos que se pueden asignar masivamente.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nombre_entrega',
+        'nombre_negocio',
+        'fecha',
+        'tipo_boton',
+        'observaciones',
+        'componentes',
+    ];
 }
